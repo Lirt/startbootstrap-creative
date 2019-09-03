890ee68bfee9d1282dfde971e846ccb30f768890
Ondrej Vasko <ondrej.vaskoo@gmail.com>
2019-09-03 02:46:43 +0200

    Fix instagram script paths and expansions
    Signed-off-by: Ondrej Vasko <ondrej.vaskoo@gmail.com>

--- instagram-download.sh
+++ instagram-download.sh
@@ -5,22 +5,22 @@ if [ -z "${ACCESS_TOKEN}" ] || [ -z "${DESTINATION_DIR}" ]; then
   exit 1
 fi
 
-LOG_DIR="/var/www/html/bezanka/"
+LOG_DIR="/var/www/html/bezanka"
 
 # Download list of latest Instagram photos
 curl --silent "https://api.instagram.com/v1/users/self/media/recent?access_token=${ACCESS_TOKEN}&count=6" \
      | tr ',' '\n' \
      | grep -A4 'standard_resolution' \
      | grep "url.*\.jpg" \
-     | grep -o "https://.*\.jpg.*cdninstagram\.com" > '/tmp/instagram_last_pics.txt'
+     | grep -o "https://.*\.jpg.*cdninstagram\.com" > "${LOG_DIR}/instagram_last_pics.txt"
 
 # Check if there are new photos
 #   If yes, download them and move list to backup file
 id=1
-if ! diff "/${LOG_DIR}/instagram_last_pics.txt" "/${LOG_DIR}/instagram_last_pics.bck.txt" || [ ! -f "${DESTINATION_DIR}/instagram-photo-6.jpg" ]; then
+if [ ! -f "${LOG_DIR}/instagram_last_pics.txt" ] || ! diff "${LOG_DIR}/instagram_last_pics.txt" "${LOG_DIR}/instagram_last_pics.bck.txt" || [ ! -f "${DESTINATION_DIR}/instagram-photo-6.jpg" ]; then
   while read -r link; do
     wget "${link}" -O "${DESTINATION_DIR}/instagram-photo-${id}.jpg"
     ((++id))
-  done < "/${LOG_DIR}/instagram_last_pics.txt"
-  mv "/${LOG_DIR}/instagram_last_pics.txt" "/${LOG_DIR}/instagram_last_pics.bck.txt"
+  done < "${LOG_DIR}/instagram_last_pics.txt"
+  mv "${LOG_DIR}/instagram_last_pics.txt" "${LOG_DIR}/instagram_last_pics.bck.txt"
 fi
